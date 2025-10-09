7aee38b2378a1d7e1ddb3ebbac283088d3c8d9c8
elibarranca <elisabarranca.99@gmail.com>
2025-10-08 21:32:53 -0600

    change location of documents

--- week_4/ponder/modal.js
+++ week_4/ponder/modal.js
@@ -1,11 +1,11 @@
 
-const gallery = document.querySelector('.gallery');
-const modal = document.querySelector('dialog');
-const modalImage = modal.querySelector('img');
-const closeButton = modal.querySelector('.close-viewer');
+const gallery = document.querySelector(".gallery");
+const modal = document.querySelector("dialog");
+const modalImage = modal.querySelector("img");
+const closeButton = modal.querySelector(".close-viewer');
 
 // Event listener for opening the modal
-gallery.addEventListener('click', openModal);
+gallery.addEventListener("click", openModal);
 
 function openModal(e) {
     console.log(e)    
@@ -15,18 +15,18 @@ function openModal(e) {
     const alt = img.getAttribute("alt");
     const full = src.replace("sm", "full");
 
-    modalImage.src =
+    modalImage.src = full;
     modalImage.alt = alt;
 
     modal.showModal();
 }
 // Close modal on button click
-closeButton.addEventListener('click', () => {
+closeButton.addEventListener("click", () => {
     modal.close();
 });
 
 // Close modal if clicking outside the image
-modal.addEventListener('click', (event) => {
+modal.addEventListener("click", (event) => {
     if (event.target === modal) {
         modal.close();
     }
