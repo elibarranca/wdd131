cc445d9ac706566b8b1b59666dc7f50d7a1c9fc4
elibarranca <elisabarranca.99@gmail.com>
2025-11-17 12:47:05 -0700

    class

---
+++ week_10/ponder/sortfilter.js
@@ -0,0 +1,16 @@
+nums =[4, 5, 3, 1];
+
+console.log(nums.sort());
+
+
+const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango" ]
+
+let lowerList.map(function(fruit){
+    return fruit toLowerCase();
+
+})
+console.log(simpleList = simpleList.map(funciton(fruit){
+    return fruit.toLowerCase();
+})
+
+console.log(lowerList.sort());
